7058f457bfde1770f50852e236da23c723ef6260
ryann254 <keddelyronjoz@gmail.com>
2024-07-08 10:26:32 +0300

    feat: add routes, services, and models folder

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import httpStatus from 'http-status';
 
 dotenv.config();
 
 const app = express();
 
+// DO NOT REMOVE: Used to ping if the server is still up and running.
 app.get('/', (req, res) => {
-  res.send('Hello From Surveva Backend!');
+  return res.status(httpStatus.OK).send('Hello From Surveva Backend!');
 });
 
 const PORT = 5000 || process.env.PORT;
